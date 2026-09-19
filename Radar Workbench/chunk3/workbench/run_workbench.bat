260363ea8b69bc18de9a5cdae80d3c5114f7b936
Robinante <robinsond950@gmail.com>
2026-09-19 17:00:07 -0400

    Flatten Radar Workbench: promote chunk4 to root, remove chunk3

--- Radar Workbench/chunk3/workbench/run_workbench.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM OpenFlight Radar Bench Workbench -- offline analysis of saved captures.
-REM Needs: pip install streamlit matplotlib
-cd /d "%~dp0\.."
-python -m streamlit run workbench\app.py
